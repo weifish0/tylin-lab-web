3f72d2fbd8f66babfe4d4f66ff6c47b240d461c0
weifish0 <will060710@gmail.com>
2026-04-17 01:31:49 +0800

    ci: add github actions deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import path from "path";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // Get the site URL from environment variables, or use the default value if not set
 // Note: After the first deployment, be sure to set the correct PUBLIC_SITE_URL in the .env file
-const siteUrl = import.meta.env.PUBLIC_SITE_URL || 'https://tylinlab.site/';
+const siteUrl = import.meta.env.PUBLIC_SITE_URL;
 
 // https://astro.build/config
 export default defineConfig({
