78fd423de35056d94540a9801ca903bff748e2d7
weifish0 <will060710@gmail.com>
2026-04-17 01:49:29 +0800

    fix: load public site url env

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { fileURLToPath } from "url";
 import path from "path";
+import { loadEnv } from "vite";
 
+const env = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // Get the site URL from environment variables, or use the default value if not set
 // Note: After the first deployment, be sure to set the correct PUBLIC_SITE_URL in the .env file
-const siteUrl = import.meta.env.PUBLIC_SITE_URL;
+const siteUrl = env.PUBLIC_SITE_URL;
 
 // https://astro.build/config
 export default defineConfig({
